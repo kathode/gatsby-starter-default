0c91e388956cd79a67502a8646a9f935dd6e5d97
kathode <80877706+kathode@users.noreply.github.com>
2021-04-21 17:16:59 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
+    
     <h1>Traffic Lights</h1>
     
     <StaticImage
